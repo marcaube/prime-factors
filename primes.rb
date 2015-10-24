f40521c3f17ee2464c06c6c84aff629eac8bdcec
marcaube <marcaube@users.noreply.github.com>
2015-10-24 15:32:30 -0400

    Add Ruby source code

---
+++ primes.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+
+$number = ARGV[0].to_i
+$candidate = 2
+
+while $number > 1 do
+    while $number % $candidate == 0 do
+        puts("#$candidate")
+        $number /= $candidate
+    end
+    $candidate += 1
+end

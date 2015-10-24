792539e544549e10a0230ada670882252086180b
marcaube <marcaube@users.noreply.github.com>
2015-10-24 18:59:31 -0400

    Output factor directly as int

--- primes.rb
+++ primes.rb
@@ -5,7 +5,7 @@ $candidate = 2
 
 while $number > 1 do
     while $number % $candidate == 0 do
-        puts("#$candidate")
+        puts($candidate)
         $number /= $candidate
     end
     $candidate += 1
